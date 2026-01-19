d4a0aedbb3ec526e59538e3f014704dfa3b3d69f
mruthenbury-source <mruthenbury@googlemail.com>
2026-01-19 20:01:41 +0000

    Delete api/src/index.js

--- api/src/index.js
+++
@@ -1,7 +0,0 @@
-import "./functions/bootstrap.js";
-import "./functions/save.js";
-import "./functions/tick.js";
-import "./functions/health.js";
-import "./functions/backupWeekly.js";
-import "./functions/chat.js";
-import "./functions/state.js";
