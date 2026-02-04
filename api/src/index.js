85a64d60d6c8e077987e41e37f12f6cecae7f017
mruthenbury-source <mruthenbury@googlemail.com>
2026-02-04 14:25:48 +0000

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,9 @@
-// api/src/index.js
-// Never let one broken module prevent all routes from registering.
-
-try {
-  await import("./functions/bootstrap.js");
-} catch (e) {
-  console.error("bootstrap.js failed to load (continuing):", e);
-}
-
+import "./functions/bootstrap.js";
 import "./functions/save.js";
 import "./functions/tick.js";
 import "./functions/health.js";
 import "./functions/backupWeekly.js";
 import "./functions/chat.js";
-import "./functions/exportPdf.js";
 import "./functions/state.js";
 import "./functions/meta.js";
 
