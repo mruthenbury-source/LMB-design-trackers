b29cff72bd697433ddd0d3751bf974dd597f92d9
mruthenbury-source <mruthenbury@googlemail.com>
2026-02-04 08:13:05 +0000

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,12 @@
-import "./functions/bootstrap.js";
+// api/src/index.js
+// Never let one broken module prevent all routes from registering.
+
+try {
+  await import("./functions/bootstrap.js");
+} catch (e) {
+  console.error("bootstrap.js failed to load (continuing):", e);
+}
+
 import "./functions/save.js";
 import "./functions/tick.js";
 import "./functions/health.js";
@@ -7,3 +15,4 @@ import "./functions/chat.js";
 import "./functions/exportPdf.js";
 import "./functions/state.js";
 import "./functions/meta.js";
+
